09fc1f586d0a175e26272cb3d63c7ff5cf597fbb
Jaros-777 <filip.jarocki@wp.pl>
2025-03-20 13:29:31 +0100

    Prepared a project for development

--- Rently-frontend/src/App.jsx
+++ Rently-frontend/src/App.jsx
@@ -1,42 +1,11 @@
-import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios';
+
 
 function App() {
-  const [message, setMessage] = useState("First message")
 
-  useEffect(()=>{
-    axios.get("http://localhost:8080/api/hello")
-    .then(response => {
-        console.log(response.data);
-        setMessage(response.data);
-        })
-    .catch(error => console.log(error))
-  },[])
-
-   const sendMessage = () =>{
-        axios.post("http://localhost:8080/api/button", "Od reacta",
-        {
-            headers:{
-                'Content-Type': 'application/json',
-            }
-        })
-        .then(response => {
-                      console.log('Odpowiedź z backendu:', response.data);
-                      setMessage(response.data); // Ustaw odpowiedź na ekranie
-                  })
-                  .catch(error => {
-                      console.error('Błąd:', error);
-                  });
-
-   }
 
   return (
     <>
-      <h1>{message}</h1>
-      <button onClick={sendMessage} >Send message</button>
+        <p>Start</p>
     </>
   )
 }
